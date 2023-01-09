451f9aa0bf3e095bf5d6086048964b5580c601c1
antonygiomardev <antony.hernandez@atomchat.io>
2023-01-09 16:11:04 -0600

    feat: added env config :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,16 @@ import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI),
 
     PokemonModule,
 
